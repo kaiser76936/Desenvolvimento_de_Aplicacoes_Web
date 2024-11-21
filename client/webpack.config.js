2f78da711c551987e24e7e2854c0a893e6df3e02
kaiser76936 <a76936@ualg.pt>
2024-11-21 20:46:16 +0000

    v0.3.1

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
   devServer: {
     static: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000,
+    port: 6000,
     proxy: [
       {
         context: ['/api'],
