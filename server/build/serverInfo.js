260c456c18dead51679a2447860e7bdbbed7fc9a
kaiser76936 <a76936@ualg.pt>
2024-11-25 22:13:41 +0000

    v0.7.7

--- server/build/serverInfo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serverInfo = void 0;
-exports.serverInfo = {
-    name: 'REST API Server',
-    version: '1.0.0',
-    description: 'This is a REST API server for managing products, users, and orders'
-};
